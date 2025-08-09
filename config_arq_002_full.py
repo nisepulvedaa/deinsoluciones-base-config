6a585de103c919ca832cf4a3da96904d65a935c9
Nicolas Sepulveda <ni.sepulvedaa@gmail.com>
2025-08-09 18:23:11 -0400

    base para optimización en proceso de desarrollo

---
+++ config_arq_002_full.py
@@ -0,0 +1,16 @@
+nombre_proceso="" #nombre-proceso
+version_arquetipo_raw="" #slim|full
+periodicidad="diario"# diario|mensual|esporadico
+##ORIGIN
+buckets_detino=""
+path_name=""
+nombre_archivo=""
+##RAW
+proyecto="deinsoluciones-serveless"
+dataset_raw_zone=""
+nombre_tabla_raw=""
+campo_fecha_tabla_raw=""
+fecha_ejecucion=""
+ddl_raw=""
+##CORREOS
+correos_destinatarios=""
